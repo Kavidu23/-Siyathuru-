6424e12d03afab8960dc4dc10fe2518987c7d3f4
Kavidu23 <10952412@students.plymouth.ac.uk>
2025-10-05 17:52:25 +0530

    test: add RouterTestingModule to SuperadminComponent tests for improved routing support

--- frontend/src/app/superadmin/superadmin.component.spec.ts
+++ frontend/src/app/superadmin/superadmin.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SuperadminComponent } from './superadmin.component';
 
@@ -8,9 +9,9 @@ describe('SuperadminComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SuperadminComponent]
+      imports: [SuperadminComponent, RouterTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(SuperadminComponent);
     component = fixture.componentInstance;
