d076dcc8480fd3f44c065611314c25707d4bc1c1
Kavidu23 <10952412@students.plymouth.ac.uk>
2026-02-03 22:15:47 +0530

    feat(frontend):create about, blog and pro pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,6 +10,9 @@ import { ChatboxComponent } from './chatbox/chatbox.component';
 import { ChartDashboardComponent } from './charts/charts.component';
 import { SuperadminComponent } from './superadmin/superadmin.component';
 import { ManagementComponent } from './management/management.component';
+import { BlogComponent } from './blog/blog.component';
+import { ProComponent } from './pro/pro.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, // redirect '/' to '/home'
@@ -24,5 +27,8 @@ export const routes: Routes = [
   { path: 'chart', component: ChartDashboardComponent },
   { path: 'superadmin', component: SuperadminComponent },
   { path: 'management', component: ManagementComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'pro', component: ProComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: 'home' },
 ];
