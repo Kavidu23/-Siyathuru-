23dd3d80310361c840d3d816b36c3885f378819f
Kavidu23 <10952412@students.plymouth.ac.uk>
2026-02-25 17:52:01 +0530

    fix(frontend):fix about, blog and pro pages not showing the footer issue

--- frontend/src/app/pro/pro.component.ts
+++ frontend/src/app/pro/pro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-pro',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './pro.component.html',
   styleUrl: './pro.component.css',
 })
