bdd57bc6d32fa68d52fe90dde95446414a065f3a
Kavidu23 <10952412@students.plymouth.ac.uk>
2026-05-01 18:50:55 +0530

    test(frontend):fix the test failed

--- frontend/src/app/modal.service.spec.ts
+++ frontend/src/app/modal.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ModalService } from './services/modal.service';
+import { UserService } from './services/user.service';
 
 describe('ModalService', () => {
   let service: ModalService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            getCurrentUser: () => null,
+          },
+        },
+      ],
+    });
     service = TestBed.inject(ModalService);
   });
 
